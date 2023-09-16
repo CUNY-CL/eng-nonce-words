e15b0a8445b07f9f334d2f4980aa27847e8b2b11
Kyle Gorman <kylebgorman@gmail.com>
2023-09-15 21:14:20 -0400

    Work on monosyllables.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ To use, run:
 
 ```
 pip install -r requirements.txt
-citylex --wikipron_us && rm -f citylex.tsv
+citylex --wikipron-us && rm -f citylex.tsv
 ./generate.py
 # Annotate for lexicality.
 ./stratify.py
--- generate.py
+++ generate.py
@@ -6,16 +6,52 @@ Subsequent steps will remove actual lexical items and balance the stimuli
 according to our assumptions about grammaticality.
 """
 
+import csv
 import dataclasses
 import logging
 
-from typing import List, Optional, Set
+from typing import Iterator, List, Optional, Set
 
 import citylex
 
 
 MONOSYLLABLES = "monosyllables.tsv"
 DISYLLABLES = "disyllables.tsv"
+
+
+# Onsets.
+VOICELESS_STOPS = ["p", "t", "k"]
+VOICED_STOPS = ["b", "d", "g"]
+STOPS = VOICELESS_STOPS + VOICED_STOPS
+# Leaving off /θ, ð/ because of the obvious spelling issues.
+FRICATIVES = ["f", "s", "h"]
+LIQUID = ["l", "r"]
+# ŋ does not occur word-initially
+PLAIN_NASALS = ["m", "n"]
+SIMPLE_ONSETS = STOPS + ["s"] + FRICATIVES + PLAIN_NASALS
+
+# For simplicity, I avoid the low-back vowels.
+# I put aside /ɔɪ/ as it's pretty rare.
+TENSE_NUCLEI = ["iː", "uː", "eɪ", "oʊ", "aɪ", "aʊ"]
+# Of course some dialects have a tense variant of [æ].
+LAX_NUCLEI = ["ɪ", "ʊ", "ɛ", "æ"]
+NUCLEI = TENSE_NUCLEI + LAX_NUCLEI
+
+
+# Codas.
+NASAL_CODAS = ["m", "n", "ŋ"]
+# I leave off voiceless variants to simplify the place checking.
+STOP_CODAS = ["p", "t", "k"]
+CODAS = NASAL_CODAS + STOP_CODAS
+
+
+# Maps stop onto a homo-organic nasal.
+STOP_TO_NASAL = {
+    "p": "m",
+    "b": "m",
+    "t": "n",
+    "d": "n",
+}
 
 
 class Error(Exception):
@@ -30,8 +66,18 @@ class Monosyllable:
     shape: Optional[str]
 
     @property
+    def transcription(self) -> str:
+        return self.onset + self.nucleus + self.coda
+
+    @property
     def line(self) -> List[str]:
-        return [self.onset, self.nucleus, self.coda, self.shape]
+        return [
+            self.onset,
+            self.nucleus,
+            self.coda,
+            self.shape,
+            self.transcription,
+        ]
 
 
 @dataclasses.dataclass
@@ -41,26 +87,75 @@ class Bisyllable:
     shape: str
 
     @property
+    def transcription(self) -> str:
+        return f"{self.syl1.transcription}.{self.syl2.transcription}"
+
+    @property
     def line(self) -> List[str]:
         return [*self.syl1.line, *self.syl2.line, self.shape]
 
 
+def _monosyllables() -> Iterator[Monosyllable]:
+    # CVC.
+    for onset in SIMPLE_ONSETS:
+        for nucleus in NUCLEI:
+            for coda in CODAS:
+                if coda.startswith(onset[0]):
+                    continue
+                yield Monosyllable(onset, nucleus, coda, "CVC")
+    # sCVC.
+    # I could do sl here too, but not a lot gained from it.
+    for stop in VOICELESS_STOPS:
+        for nucleus in NUCLEI:
+            for coda in CODAS:
+                if stop == coda:
+                    continue
+                yield Monosyllable("s" + stop, nucleus, coda, "sCVC")
+    # CwVC.
+    for stop in STOPS:
+        for nucleus in LAX_NUCLEI:
+            for coda in CODAS:
+                if stop == coda:
+                    continue
+                yield Monosyllable(stop + "w", nucleus, coda, "CwVC")
+    # TlVC and TrVC.
+    for stop in ["t", "d"]:
+        for nucleus in NUCLEI:
+            for coda in CODAS:
+                if stop == coda:
+                    continue
+                yield Monosyllable(stop + "l", nucleus, coda, "TlVC")
+                yield Monosyllable(stop + "ɹ", nucleus, coda, "TɹVC")
+    # Prenasal and postnasal.
+    for stop, nasal in STOP_TO_NASAL.items():
+        for nucleus in NUCLEI:
+            for coda in STOP_CODAS:
+                if stop == coda:
+                    continue
+                yield Monosyllable(stop + nasal, nucleus, coda, "CNVC")
+                yield Monosyllable(nasal + stop, nucleus, coda, "NCVC")
+
+
 def main():
     cl = citylex.read_textproto("citylex.textproto")
-    lexicon: Set[
-        str
-    ] = set()  # We store them as strings because lists aren't hashable.
+    lexicon: Set[str] = set()
     for entry in cl.entry.values():
         for pron in entry.wikipron_us_pron:
-            lexicon.add(pron)
+            lexicon.add(pron.replace(" ", ""))
     logging.info(f"{len(lexicon):,} lexicon entries")
-    """
+    filtered = 0
     with open(MONOSYLLABLES, "w") as sink:
         tsv_writer = csv.writer(sink, delimiter="\t")
-        tsv_writer.writerow(["onset", "nucleus", "coda", "shape"])
+        tsv_writer.writerow(
+            ["onset", "nucleus", "coda", "shape", "transcription"]
+        )
         for entry in _monosyllables():
+            if entry.transcription in lexicon:
+                logging.info(f"{entry.transcription} is lexical")
+                filtered += 1
+                continue
             tsv_writer.writerow(entry.line)
-    """
+    logging.info(f"{filtered:,} monosyllables filtered")
 
 
 if __name__ == "__main__":
